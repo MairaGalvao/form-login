678637b3172958c21acfd8fa5271dcbaa15bbcfa
mairaGalvao <mairagalvao01@gmail.com>
2022-06-07 16:49:01 +0300

    feat: displayed form according to the location pick from the user

--- src/App.js
+++ src/App.js
@@ -1,36 +1,79 @@
-import logo from "./logo.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+	const [position, setPosition] = useState("center");
+
+	useEffect(() => {
+		//setting the current position on mount
+		let currentPosition = localStorage.getItem("position");
+		console.log(currentPosition);
+		if (currentPosition === null) {
+			return;
+		}
+		setPosition(currentPosition);
+	}, []);
+
 	return (
-		<div>
-			<form action="/action_page.php">
-				<label for="fname">First Name</label>
-				<input
-					type="text"
-					id="fname"
-					name="firstname"
-					placeholder="Your name.."
-				/>
+		<>
+			<div className="positions-options">
+				<label>Select position</label>
 
-				<label for="lname">Last Name</label>
-				<input
-					type="text"
-					id="lname"
-					name="lastname"
-					placeholder="Your last name.."
-				/>
+				<button
+					onClick={() => {
+						setPosition("top-left");
+						localStorage.setItem("position", "top-left");
+					}}
+				>
+					Top Left
+				</button>
 
-				<label for="country">Country</label>
-				<select id="country" name="country">
-					<option value="australia">Australia</option>
-					<option value="canada">Canada</option>
-					<option value="usa">USA</option>
-				</select>
+				<button
+					onClick={() => {
+						setPosition("buttom-left");
+						localStorage.setItem("position", "buttom-left");
+					}}
+				>
+					Button Left
+				</button>
 
-				<input type="submit" value="Submit" />
-			</form>
-		</div>
+				<button
+					onClick={() => {
+						setPosition("center");
+						localStorage.setItem("position", "center");
+					}}
+				>
+					Center
+				</button>
+
+				<button
+					onClick={() => {
+						setPosition("buttom-right");
+						localStorage.setItem("position", "buttom-right");
+					}}
+				>
+					Button Right
+				</button>
+
+				<button
+					onClick={() => {
+						setPosition("top-right");
+						localStorage.setItem("position", "top-right");
+					}}
+				>
+					Top Right
+				</button>
+			</div>
+			<div className={position}>
+				<label>User Name</label>
+				<input placeholder="Name" />
+
+				<label>Password</label>
+				<input placeholder="Password" />
+
+				<button>Submit</button>
+			</div>
+		</>
 	);
 }
 
